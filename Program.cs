7301d263e3eea1060f6b2796f6e2c3dac395523e
spry <yoshida@spr.jp>
2016-04-24 22:22:04 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ext_yo_consoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            var ret = Console.ReadLine();
+            Console.WriteLine(ret);
+            Console.ReadKey(true);
+        }
+    }
+}
